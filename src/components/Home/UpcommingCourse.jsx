c42152195062c36e6e4068e55ac246022e958ee4
smhasanjamil <smhasanjamil23@gmail.com>
2023-06-16 23:16:48 +0600

    Upcomming course added

--- src/components/Home/UpcommingCourse.jsx
+++ src/components/Home/UpcommingCourse.jsx
@@ -1,13 +1,33 @@
-// import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
 
 
 const UpcommingCourse = () => {
     return (
-        <div>
+        <div className="my-12">
 
+<h3 className="text-center font-bold text-3xl my-24 title-styles">Upcomming Classes</h3>
 
-            
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide><img src="https://i.ibb.co/mXV8yGp/25.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/z6vVZ6c/26.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/D7p4bKN/27.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/mXV8yGp/25.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img src="https://i.ibb.co/z6vVZ6c/26.jpg" alt="" /></SwiperSlide>
+            </Swiper>
+
         </div>
     );
 };
