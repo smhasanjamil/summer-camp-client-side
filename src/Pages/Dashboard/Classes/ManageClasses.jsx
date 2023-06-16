4149f9580f5c24513535e54457156f969f42b870
smhasanjamil <smhasanjamil23@gmail.com>
2023-06-16 09:10:37 +0600

    Show all classes in the dashboard

--- src/Pages/Dashboard/Classes/ManageClasses.jsx
+++ src/Pages/Dashboard/Classes/ManageClasses.jsx
@@ -14,7 +14,78 @@ const ManageClasses = () => {
         <div>
             <h3 className="text-3xl font-bold">Total Classes : {classes.length}</h3>
 
-        </div>
+            <div>
+
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Class Image</th>
+                                <th>Class name</th>
+                                <th>Instructor name</th>
+                                <th>Instructor email</th>
+                                <th>Available seats</th>
+                                <th>Price</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {classes.map((newClass, index) => (
+                                <>
+
+                                    <tr>
+                                        <th>
+                                            <th>{index + 1}</th>
+                                        </th>
+                                        <td>
+                                            <div className="flex items-center space-x-3">
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-12 h-12">
+                                                        <img src={newClass.image} alt="Avatar Tailwind CSS Component" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>{newClass.className}</td>
+                                        <td>{newClass.instructorName}</td>
+                                        <td>{newClass.email}</td>
+                                        <td>{newClass.availableSeat}</td>
+                                        <td>${newClass.price}</td>
+                                        {/* <td>{newClass.status}</td> */}
+                                        {/* <td>
+                                            <div className="flex flex-row gap-2">
+                                                <button className="btn btn-neutral">{newClass.status}</button>
+                                                <button className="btn btn-neutral">{newClass.status}</button>
+                                                <button className="btn btn-neutral">{newClass.status}</button>
+                                            </div>
+                                        </td> */}
+                                        <td>
+                                            <div className="flex flex-row gap-2">
+                                                <button className={`btn ${newClass.status === 'pending' ? 'btn-disabled' : 'btn-primary'}`} disabled={newClass.status === 'pending'}>Pending</button>
+                                                <button className={`btn ${newClass.status === 'approved' ? 'btn-disabled' : 'btn-primary'}`} disabled={newClass.status === 'approved'}>Approve</button>
+                                                <button className={`btn ${newClass.status === 'denied' ? 'btn-disabled' : 'btn-primary'}`} disabled={newClass.status === 'denied'}>Deny</button>
+                                            </div>
+                                        </td>
+
+
+
+                                    </tr>
+                                </>
+                            ))}
+
+
+
+                        </tbody>
+
+
+                    </table>
+                </div>
+
+            </div>
+
+        </div >
     );
 };
 
